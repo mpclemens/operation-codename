fe796da050518a94a279836648deb3f683b8e8e3
Mike Clemens <mclemens@brit.co>
2018-08-02 19:15:21 -0700

    Refactor out Word/ScoredWord, linting

--- codename.py
+++ codename.py
@@ -2,51 +2,88 @@
 
 import random
 
+
 class Env():
     """
-    Encapsulate preferences for scoring
+    Encapsulate preferences for scoring and breeding words:
+
+    gene_len: letter sequence length for a gene
+    word_min/word_max: preferred bounds for new words
+    phrase_len: how many words make up a phrase
+    gene_scores: dict of {gene: score} mappings
+    pop_size: max population size of each generation
     """
-    def __init__(self, gene_len=3, word_min=3, word_max=9, phrase_len=2):
+    def __init__(self, gene_len=3, word_min=3, word_max=9,
+                 phrase_len=2, pop_size=100):
         self.gene_len = gene_len
         self.word_min = word_min
         self.word_max = word_max
         self.phrase_len = 2
         self.gene_scores = {}
+        self.pop_size = pop_size
+
 
 class Word():
-    SCRABBLE = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
-                "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
-                "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
-                "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
-                "x": 8, "z": 10}
-
-    def __init__(self, env, w, compute=True):
+    """
+    Base class for words, with methods to extract genes and cross-breed
+    """
+    def __init__(self, env, w):
         """
-        Define a new word using environment settings for scoring. By default,
-        the score is computed on creation, but for efficiency, this can be
-        disabled by the 'compute' parameter
+        Define a new word, saving settings for gene splitting.
         """
         self.env = env
         self.word = w.lower()
-        if compute:
-            self.compute_score()
-        else:
-            self.score = 0
 
     def genes(self):
         """
         Return a list of genes (sequential letter substrings) of the
         given length
         """
-        return [self.word[i:i+self.env.gene_len] for i in range(0,len(self.word) - self.env.gene_len + 1)]
+        return [self.word[i:i+self.env.gene_len]
+                for i in range(0, len(self.word) - self.env.gene_len + 1)]
 
-    def compute_score(self):
+    def breed(parent1, parent2):
         """
-        Set the internal score variables based on the stored environment
+        Given two parent Words (the caller plus one other), generate a new
+        string made from a random smattering of the genes of the parents,
+        just like real life!
         """
-        self.scrabble_score = sum([Word.SCRABBLE.get(l,0) for l in self.word])
-        self.gene_score = sum([self.env.gene_scores.get(g,0) for g in self.genes()])
-        self.score = self.scrabble_score + self.gene_score - max(0, self.env.word_min - len(self.word)) - max(0, len(self.word) - self.env.word_max)
+        pool = parent1.genes() + parent2.genes()
+        e = parent1.env
+
+        if not pool:
+            return ""
+
+        # decides how many genes go into the "baby" word
+        baby_min = min(int(e.word_min/e.gene_len + 0.5), e.gene_len)
+        baby_max = e.word_max//e.gene_len
+        baby_len = random.randint(baby_min, baby_max)
+        return "".join(random.sample(pool, baby_len))
+
+
+class ScoredWord(Word):
+    """
+    Derivative of Word: a word with a score set at create time
+    """
+    SCRABBLE = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+                "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+                "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+                "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+                "x": 8, "z": 10}
+
+    def __init__(self, env, w):
+        """
+        Define a new word using environment settings for scoring.
+        """
+        super().__init__(env, w)
+        self.scrabble_score = sum([ScoredWord.SCRABBLE.get(l, 0)
+                                   for l in self.word])
+        self.gene_score = sum([self.env.gene_scores.get(g, 0)
+                               for g in self.genes()])
+        self.score = self.scrabble_score + self.gene_score - \
+            max(0, self.env.word_min - len(self.word)) - \
+            max(0, len(self.word) - self.env.word_max)
+
 
 class Phrase():
     """
@@ -57,16 +94,21 @@ class Phrase():
     def __init__(self, env, words):
         self.env = env
         self.words = words
-        self.compute_score()
+        self.score = sum([w.score for w in words])
 
-    def compute_score(self):
+    def breed(phrase1, phrase2):
         """
-        Given a desired gene length, and min and max letter lengths
-        of the words (inclusive), compute the overall phrase score.
-        Words too long or too short lose one point per letter outside
-        the thresholds.
+        Breed corresponding words between the two phrases, generating a new
+        phrase. If the phrases are of unequal length, the new phrase will
+        be of the same length of the shortest.
         """
-        self.score = sum([w.score for w in self.words])
+        baby_len = min(len(phrase1.words), len(phrase2.words))
+        baby_words = []
+        for i in range(baby_len):
+            baby_words.append(phrase1.words[i].breed(phrase2.words[i]))
+
+        return Phrase(phrase1.env, baby_words)
+
 
 class Codename():
 
@@ -90,7 +132,7 @@ class Codename():
         """
         seen_genes = {}
         for c in self.corpus:
-            w = Word(self.env, c, compute=False)
+            w = Word(self.env, c)
             for g in w.genes():
                 if g in seen_genes:
                     seen_genes[g] += 1
@@ -98,6 +140,7 @@ class Codename():
                     seen_genes[g] = 1
         self.env.gene_scores = seen_genes
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     p = Env()
     c = Codename(p)
--- codename_test.py
+++ codename_test.py
@@ -2,10 +2,44 @@
 
 import unittest
 
-from codename import Env, Word, Phrase, Codename
+from codename import Env, Word, ScoredWord, Phrase, Codename
 
 
 class TestCodename(unittest.TestCase):
+
+    def test_word(self):
+        e = Env()
+
+        w = Word(e, "hello")
+        self.assertIsNotNone(w.word, msg="expected word defined")
+        self.assertIsNotNone(w.genes(), msg="expected genes defined")
+
+    def test_word_breeding(self):
+        e = Env()
+
+        w1 = Word(e, "hello")
+        w2 = Word(e, "world")
+
+        self.assertNotEqual("", w1.breed(w2),
+                            msg="expected nonempty baby word 1")
+
+        w1 = Word(e, "")
+        w2 = Word(e, "world")
+
+        self.assertNotEqual("", w1.breed(w2),
+                            msg="expected nonempty baby word 2")
+
+        w1 = Word(e, "hello")
+        w2 = Word(e, "")
+
+        self.assertNotEqual("", w1.breed(w2),
+                            msg="expected nonempty baby word 3")
+
+        w1 = Word(e, "")
+        w2 = Word(e, "")
+
+        self.assertEqual("", w1.breed(w2),
+                         msg="expected empty baby word")
 
     def test_scrabble_and_gene_scores(self):
         e = Env()
@@ -25,7 +59,7 @@ class TestCodename(unittest.TestCase):
         }
 
         for w, s in test_data.items():
-            word = Word(e, w)
+            word = ScoredWord(e, w)
             scrabble, gene = s[:]
             self.assertEqual(scrabble, word.scrabble_score,
                              msg="{} scrabble score".format(w))
@@ -45,9 +79,9 @@ class TestCodename(unittest.TestCase):
         #
         # 'd' scores 2 points, and going over or under the word amounts costs
         # one point
-        base_word = Word(e, "dddd")
-        short_word = Word(e, "dd")
-        long_word = Word(e, "dddddddd")
+        base_word = ScoredWord(e, "dddd")
+        short_word = ScoredWord(e, "dd")
+        long_word = ScoredWord(e, "dddddddd")
 
         self.assertNotEqual(0, base_word.score,
                             msg="expected test word to have a score")
@@ -67,8 +101,8 @@ class TestCodename(unittest.TestCase):
     def test_phrase_score_is_sum(self):
         e = Env()
 
-        w1 = Word(e, "hello")
-        w2 = Word(e, "world")
+        w1 = ScoredWord(e, "hello")
+        w2 = ScoredWord(e, "world")
         p = Phrase(e, [w1, w2])
 
         self.assertEqual(w1.score + w2.score, p.score,
@@ -80,7 +114,8 @@ class TestCodename(unittest.TestCase):
 
         self.assertEqual(0, len(e.gene_scores), "expected no gene scores yet")
 
-        c = Codename(e, corpus=["apple", "banana", "cherry", "cheese", "chalk"])
+        c = Codename(e,
+                     corpus=["apple", "banana", "cherry", "cheese", "chalk"])
 
         self.assertNotEqual(0, len(e.gene_scores), "expected gene scores")
 
@@ -91,13 +126,6 @@ class TestCodename(unittest.TestCase):
         self.assertTrue(e.gene_scores.get('ch') > e.gene_scores.get('pp'),
                         "expected 'ch' score > 'pp' score")
 
-    def test_uncomputed_words(self):
-        e = Env()
-        no_score = Word(e, "foobar", compute=False)
-        scored = Word(e, "foobar")
-
-        self.assertEqual(0, no_score.score, "expected no computed score")
-        self.assertNotEqual(0, scored.score, "expected a computed score")
 
 if __name__ == "__main__":
     unittest.main()
